a116c5d066fc50c3d2f67948ba40da0cadf1d2d7
Marcus <marcus0905@hotmail.com>
2024-11-28 01:16:24 -0300

    Iniciando formulario cliente

---
+++ Locadora/Frm_clientes.Designer.cs
@@ -0,0 +1,473 @@
+﻿namespace Locadora
+{
+    partial class Frm_clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripButton2 = new ToolStripButton();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBox9 = new TextBox();
+            maskedTextBox1 = new MaskedTextBox();
+            groupBox2 = new GroupBox();
+            label11 = new Label();
+            label12 = new Label();
+            maskedTextBox2 = new MaskedTextBox();
+            maskedTextBox3 = new MaskedTextBox();
+            dateTimePicker1 = new DateTimePicker();
+            label13 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            maskedTextBox4 = new MaskedTextBox();
+            maskedTextBox5 = new MaskedTextBox();
+            label14 = new Label();
+            textBox3 = new TextBox();
+            dataGridView1 = new DataGridView();
+            toolStrip1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripSeparator1, toolStripButton2 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(921, 58);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.AutoSize = false;
+            toolStripButton1.Image = Properties.Resources.novo;
+            toolStripButton1.ImageScaling = ToolStripItemImageScaling.None;
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(98, 55);
+            toolStripButton1.Text = "Novo";
+            toolStripButton1.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 38);
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.AutoSize = false;
+            toolStripButton2.Image = Properties.Resources.salvar;
+            toolStripButton2.ImageScaling = ToolStripItemImageScaling.None;
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(98, 55);
+            toolStripButton2.Text = "Salvar";
+            toolStripButton2.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(98, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Registro:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(198, 91);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(55, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(120, 133);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Nome:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(198, 130);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(418, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(3, 166);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 20);
+            label3.TabIndex = 1;
+            label3.Text = "Data de Nascimento:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(45, 205);
+            label4.Name = "label4";
+            label4.Size = new Size(135, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Numero da cnh:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(198, 202);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(235, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(118, 245);
+            label5.Name = "label5";
+            label5.Size = new Size(62, 20);
+            label5.TabIndex = 1;
+            label5.Text = "Bairro:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(198, 245);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(418, 23);
+            textBox5.TabIndex = 3;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(110, 288);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 20);
+            label6.TabIndex = 1;
+            label6.Text = "Cidade:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(198, 285);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(418, 23);
+            textBox6.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(118, 333);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 20);
+            label7.TabIndex = 1;
+            label7.Text = "Email:";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(198, 330);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(418, 23);
+            textBox7.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Location = new Point(648, 133);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(261, 81);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Documentos";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(438, 202);
+            label8.Name = "label8";
+            label8.Size = new Size(92, 20);
+            label8.TabIndex = 1;
+            label8.Text = "Categoria:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(6, 19);
+            label9.Name = "label9";
+            label9.Size = new Size(41, 20);
+            label9.TabIndex = 1;
+            label9.Text = "RG:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(6, 52);
+            label10.Name = "label10";
+            label10.Size = new Size(48, 20);
+            label10.TabIndex = 1;
+            label10.Text = "CPF:";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(65, 19);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(128, 23);
+            textBox9.TabIndex = 2;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(65, 52);
+            maskedTextBox1.Mask = "999,999,999-99";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(128, 23);
+            maskedTextBox1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(maskedTextBox3);
+            groupBox2.Controls.Add(maskedTextBox2);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Location = new Point(648, 245);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(261, 81);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Contato";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(6, 52);
+            label11.Name = "label11";
+            label11.Size = new Size(94, 20);
+            label11.TabIndex = 1;
+            label11.Text = "Telefone2:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(6, 19);
+            label12.Name = "label12";
+            label12.Size = new Size(94, 20);
+            label12.TabIndex = 1;
+            label12.Text = "Telefone1:";
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(106, 17);
+            maskedTextBox2.Mask = "(99) 0000-0000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(128, 23);
+            maskedTextBox2.TabIndex = 3;
+            // 
+            // maskedTextBox3
+            // 
+            maskedTextBox3.Location = new Point(106, 49);
+            maskedTextBox3.Mask = "(99) 0000-0000";
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.Size = new Size(128, 23);
+            maskedTextBox3.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            dateTimePicker1.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(508, 94);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(108, 22);
+            dateTimePicker1.TabIndex = 5;
+            dateTimePicker1.Value = new DateTime(2024, 11, 28, 1, 6, 47, 0);
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.Location = new Point(371, 96);
+            label13.Name = "label13";
+            label13.Size = new Size(131, 20);
+            label13.TabIndex = 1;
+            label13.Text = "Data  Cadastro";
+            // 
+            // maskedTextBox4
+            // 
+            maskedTextBox4.Location = new Point(198, 166);
+            maskedTextBox4.Mask = "00/00/0000";
+            maskedTextBox4.Name = "maskedTextBox4";
+            maskedTextBox4.Size = new Size(235, 23);
+            maskedTextBox4.TabIndex = 6;
+            // 
+            // maskedTextBox5
+            // 
+            maskedTextBox5.Location = new Point(537, 199);
+            maskedTextBox5.Mask = "0/0";
+            maskedTextBox5.Name = "maskedTextBox5";
+            maskedTextBox5.Size = new Size(79, 23);
+            maskedTextBox5.TabIndex = 7;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.Location = new Point(437, 165);
+            label14.Name = "label14";
+            label14.Size = new Size(74, 20);
+            label14.TabIndex = 1;
+            label14.Text = "Genero:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(508, 163);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(108, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 359);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(897, 261);
+            dataGridView1.TabIndex = 8;
+            // 
+            // Frm_clientes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(921, 632);
+            Controls.Add(dataGridView1);
+            Controls.Add(maskedTextBox5);
+            Controls.Add(maskedTextBox4);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(textBox1);
+            Controls.Add(label14);
+            Controls.Add(label8);
+            Controls.Add(label13);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(toolStrip1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Frm_clientes";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Frm_clientes";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripButton toolStripButton2;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox7;
+        private GroupBox groupBox1;
+        private Label label8;
+        private MaskedTextBox maskedTextBox1;
+        private Label label10;
+        private Label label9;
+        private TextBox textBox9;
+        private GroupBox groupBox2;
+        private MaskedTextBox maskedTextBox3;
+        private MaskedTextBox maskedTextBox2;
+        private Label label11;
+        private Label label12;
+        private DateTimePicker dateTimePicker1;
+        private Label label13;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MaskedTextBox maskedTextBox4;
+        private MaskedTextBox maskedTextBox5;
+        private Label label14;
+        private TextBox textBox3;
+        private DataGridView dataGridView1;
+    }
+}
